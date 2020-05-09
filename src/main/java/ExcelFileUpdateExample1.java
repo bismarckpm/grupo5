c52e32968b3bf76e5d79010dc56a2a7f0e991adf
Moises <moiseslsth2204@gmail.com>
2020-05-09 19:37:43 -0400

    Historia de usairo B v1

--- src/main/java/ExcelFileUpdateExample1.java
+++ src/main/java/ExcelFileUpdateExample1.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.CacheResponse;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -27,21 +28,86 @@ public class ExcelFileUpdateExample1 {
 		try {
 			FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
 			Workbook workbook = WorkbookFactory.create(inputStream);
-
 			Sheet sheet = workbook.getSheetAt(0);
+
+			int bookCount = 1;
+			int rowCount = sheet.getLastRowNum();
+			int cellCount = 0;
+			int i = 0;
+			int j =0;
+			int k = 0;
+			int m = 0;
+			
 
 			Object[][] bookData = {
 					{"El que se duerme pierde", "Tom Peter", 16},
 					{"Sin lugar a duda", "Ana Gutierrez", 26},
 					{"El arte de dormir", "Nico", 32},
 					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41},
+					{"El que se duerme pierde", "Tom Peter", 16},
+					{"Sin lugar a duda", "Ana Gutierrez", 26},
+					{"El arte de dormir", "Nico", 32},
+					{"Buscando a Nemo", "Humble Po", 41}
 			};
 
-			int rowCount = sheet.getLastRowNum();
+			
 
 			for (Object[] aBook : bookData) {
+				
 				Row row = sheet.createRow(++rowCount);
-
+				
 				int columnCount = 0;
 				
 				Cell cell = row.createCell(columnCount);
@@ -56,7 +122,46 @@ public class ExcelFileUpdateExample1 {
 					}
 				}
 
+				if (rowCount == 30){
+					String bookBombre = "Java Books" + ++bookCount;
+					sheet = workbook.createSheet(bookBombre);
+					rowCount = 0;
+				}
 			}
+
+			int sheetCount = workbook.getNumberOfSheets();
+
+			for ( j = 0 ; j < sheetCount; ++j){
+
+				sheet = workbook.getSheetAt(j);
+
+				System.out.println ("Hoja: " + sheet.getSheetName());
+				rowCount = sheet.getLastRowNum();
+
+				for ( k = 1 ; k <= rowCount; ++k){
+
+					Row row = sheet.getRow(k);
+
+					System.out.println ("Fila: " + k);
+					cellCount = row.getLastCellNum();
+
+					for ( m = 1 ; m < cellCount; ++m){
+
+						Cell nro = row.getCell(m);
+
+						if (m == 1 || m == 2){
+							System.out.println (nro.getStringCellValue());
+						}
+
+						if(m == 3){
+							System.out.println (nro.getNumericCellValue());
+						}
+
+					}				
+
+				}
+
+			};
 
 			inputStream.close();
 
@@ -71,4 +176,15 @@ public class ExcelFileUpdateExample1 {
 		}
 	}
 
+
+	public static int contarHojas(Object[][] bd){
+
+		int cantHojas = 0; 
+
+		for (Object[] aBook : bd) {
+			++cantHojas;
+		}
+
+		return cantHojas;
+	}
 }
